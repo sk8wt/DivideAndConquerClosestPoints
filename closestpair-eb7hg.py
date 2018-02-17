195f8c00529b5de97b9f5afc24d4ded57202e6d5
elibosley <Heidiheidi1>
2018-02-17 17:32:07 -0500

    updated searching function

--- closestpair-eb7hg.py
+++ closestpair-eb7hg.py
@@ -1,4 +1,5 @@
 # Elijah Bosley (eb7hg@virginia.edu)
+# Collaborated with Simmy Bhatia
 import math
 from functools import total_ordering
 from typing import List
@@ -43,6 +44,34 @@ def get_y(point: Point) -> float:
     return point.y
 
 
+def merge_sort(points):
+    if len(points) <= 1:
+        return points
+    median = int(len(points) / 2)  # Center divider
+    left = points[:median]
+    right = points[median:]
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+
+def merge(left: List[Point], right: List[Point]):
+    sorted_points = []
+    while len(left) > 0 and len(right) > 0:
+        if left[0].y < right[0].y:
+            sorted_points.append(left[0])
+            left.remove(left[0])
+        else:
+            sorted_points.append(right[0])
+            right.remove(right[0])
+    if len(left) is 0:
+        sorted_points += right
+    else:
+        sorted_points += left
+    return sorted_points
+
+
 def square(a: float) -> float:
     return a * a
 
@@ -56,20 +85,25 @@ def find_points_on_runway(runway_width: float, median: float, points: List[Point
     min_x = median - runway_width
 
     points_on_runway = [point for point in points if min_x < point.x < max_x]
-    points_on_runway.sort(key=get_y)  # TODO use merge sort
+    # points_on_runway.sort(key=get_y)  # TODO use merge sort
+
+    points_on_runway = merge_sort(points_on_runway)
+
     return points_on_runway
 
 
 def compare_runway_points(points: list) -> PointsDistancePair:
     # starting from the bottom point recurse up the list of points by checking 15 above the current item
-    max_idx = min(15, len(points))  # find the maximum value to run until
+
     best_distance_points = []
     best_distance_val = float("inf")
-    for i in range(max_idx - 1):
-        curr_distance = distance(points[i], points[i + 1])
-        if curr_distance < best_distance_val:
-            best_distance_val = curr_distance
-            best_distance_points = [points[i], points[i + 1]]
+    for i in range(len(points)):  # iterate through every point
+        max_idx = min(15, len(points))  # find the maximum value to run until
+        for j in range(i + 1, max_idx - 1):  # iterate through the 15 points above
+            curr_distance = distance(points[i], points[j])
+            if curr_distance < best_distance_val:
+                best_distance_val = curr_distance
+                best_distance_points = [points[i], points[i + 1]]
 
     return PointsDistancePair(best_distance_points, best_distance_val)
 
@@ -83,7 +117,6 @@ def closest_pair(points: list) -> PointsDistancePair:  # returns 2 points that a
     # print(str(len(left)) + " : " + str(len(right)))
     left_points_distance_pair = PointsDistancePair([], float("inf"))
     right_points_distance_pair = PointsDistancePair([], float("inf"))
-
 
     # Left side
     if len(left) < 2:
@@ -107,25 +140,24 @@ def closest_pair(points: list) -> PointsDistancePair:  # returns 2 points that a
 
     # Combine Step
     runway_width = min(left_dist, right_dist)  # this is delta
-    print(runway_width)
+
     runway_points = find_points_on_runway(runway_width, points[median].x, points)  # find all points within 2 delta
-    best_runway_points = []  # empty list for now, will be filled by following function
     runway_pair = compare_runway_points(runway_points)
     return min(left_points_distance_pair, right_points_distance_pair, runway_pair)
 
 
 def main():
-    f = open("mostlyvertical.txt", "r")
+    f = open("allvertical.txt", "r")
     points = []
     plants = f.readline().strip()
     for line in f:
         x, y = line.split()
         points.append(Point(float(x), float(y)))
 
-    print("There are {} plants".format(plants))
+    # print("There are {} plants".format(plants))
     points.sort(key=get_x)  # Start by sorting the points by x
     pair = closest_pair(points)
-    print(pair)
+    print(pair.distance)
 
 
 main()
